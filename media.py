5167215d248a862575a0c22cf2ca0661277c5f4b
Courtney Noonan <courtneynoonan@me.com>
2016-10-27 14:15:04 -0400

    refactoring code styling

--- media.py
+++ media.py
@@ -1,7 +1,14 @@
 import webbrowser
 
-class Movie() :
-    """ This class provides a way to store movie related information"""
+
+class Movie():
+    """ This class provides a way to store movie related information
+
+        Attributes:
+            movie_title (str): movie title
+            movie_storyline (str): one line synopsis of movie
+            poster_image_url (str): link to poster picture on wikipedia
+            trailer_youtube (str): link to movie trailer on youTube """
 
     VALID_RATINGS = ["G", "PG", "PG-13", "R"]
 
@@ -12,5 +19,5 @@ class Movie() :
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def show_trailer(self) :
+    def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
